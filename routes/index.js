6af7c968cbcb1313a9beb697c9c8c2efb960a270
Frank Lembalemba <franklembalemba3@gmail.com>
2023-10-11 16:26:40 +0200

    completed investPage setup

--- routes/index.js
+++ routes/index.js
@@ -48,7 +48,8 @@ router.post("/credit/page", ensureAuthenticated, (req,res) => {
             user,
             repaymentDate,
             totalRepayment,
-            amountReceived
+            amountReceived,
+            loanTerm
         })
       }
    
@@ -60,6 +61,48 @@ router.get("/invest", ensureAuthenticated, (req,res) => {
     res.render("main/invest",{
         user
     })
+})
+
+router.post("/invest/page", ensureAuthenticated, (req,res) => {
+    const user = req.user;
+    const {investmentAmount, investmentTerm}  = req.body;
+    const interestRates = {
+        1: 0.03, // 5% for a 1-week term
+        2: 0.08, // 6% for a 2-week term
+        3: 0.12, // 7% for a 3-week term
+        4: 0.18, // 8% for a 4-week term
+      };
+   
+    if (!isNaN(investmentAmount) && !isNaN(investmentTerm)) {
+        // Calculate interest based on selected investment term
+        const interestRate = interestRates[investmentTerm];
+        
+        // Calculate service fee (assuming a fixed fee of K3.00)
+        const serviceFee = 3;
+        
+        // Calculate expected returns
+        const expectedReturns = investmentAmount * interestRate;
+        
+        // Calculate total returns
+        const totalReturns = parseFloat(investmentAmount) + expectedReturns - serviceFee;
+        
+        // Calculate and display next payment date (assuming 7 days per week)
+        const today = new Date();
+        const nextPaymentDate = new Date(today.getTime() + investmentTerm * 7 * 24 * 60 * 60 * 1000);
+        const maturityDate = nextPaymentDate.toISOString().substr(0, 10);
+        
+        res.render("main/investPage",{
+            user,
+            serviceFee,
+            maturityDate,
+            totalReturns,
+            expectedReturns,
+            investmentTerm,
+            investmentAmount
+        })
+        
+      }
+    
 })
 
 router.get("/profile", ensureAuthenticated, (req,res) => {
